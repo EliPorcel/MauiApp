f2bce6305893f245e23c72f2f530004a20fc1e56
Eli Porcel <114632682+EliPorcel@users.noreply.github.com>
2024-06-08 17:04:39 -0300

    Add project files.

---
+++ MauiApp1/Views/AcercaPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiApp1.Views;
+
+public partial class AcercaPage : ContentPage
+{
+    public AcercaPage()
+    {
+        InitializeComponent();
+    }
+
+    private void btnAcercaVolver_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+}
---
+++ MauiApp1/Views/LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+namespace MauiApp1.Views;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private void btnLoginIngresar_Clicked(object sender, EventArgs e)
+    {
+        if (entLoginEmail.Text == "ites" && entLoginPassword.Text == "1234")
+        {
+            entLoginPassword.Text = string.Empty;
+            entLoginEmail.Text = string.Empty;
+            Navigation.PushAsync(new PrincipalPage());
+        }
+        else
+        {
+            entLoginPassword.Text = string.Empty;
+            entLoginEmail.Text = string.Empty;
+            DisplayAlert("Atención", "las credenciales ingresadas no son válidas", "OK");
+        }
+    }
+}
